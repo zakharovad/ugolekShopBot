d7a7c341d9f318e3c725ff64ba535e855308148e
Artem <adz@it-rkomi.ru>
2020-11-19 21:47:04 +0300

    push

--- src/keyboards/index.ts
+++ src/keyboards/index.ts
@@ -3,13 +3,19 @@ import { Markup, ContextMessageUpdate } from 'telegraf';
 import {CartCollectionProduct} from "../models/cartCollection";
 
 export const getMainKeyboard = (ctx: ContextMessageUpdate) =>{
+    const controls1 = [];
+    const controls2 = [];
     const cart = new CartCollectionProduct(ctx);
-    const mainKeyboardProducts = ctx.i18n.t('keyboards.main_keyboard.products');
-    const mainKeyboardCart = ctx.i18n.t('keyboards.main_keyboard.cart',{
+    controls1.push(ctx.i18n.t('keyboards.main_keyboard.products'));
+    controls1.push(ctx.i18n.t('keyboards.main_keyboard.cart',{
         count:cart.getCollection().length
-    });
+    }));
+
+    if(cart.getCollection().length > 0){
+        controls2.push(ctx.i18n.t('keyboards.main_keyboard.remove_cart','remove_cart'));
+    }
     let mainKeyboard: any = Markup.keyboard([
-        [mainKeyboardProducts, mainKeyboardCart]
+        controls1,controls2
     ]);
     mainKeyboard = mainKeyboard.resize().extra();
     return {
